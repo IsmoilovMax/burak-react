6d623415123fa5569e9b4aa8aed43af7a287a9e9
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-05 04:28:26 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { store } from "./app/store";
 import App from "./app/App";
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-
 import reportWebVitals from "./reportWebVitals";
 import "./css/index.css";
 import theme from "./app/MaterialTheme";
