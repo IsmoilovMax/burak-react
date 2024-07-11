3b19c69647b4474e6aba0abd688d4cb86c931de5
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-11 18:12:55 +0900

    feat: develop advertisement sectional component

--- src/app/screens/homePage/Advertisement.tsx
+++ src/app/screens/homePage/Advertisement.tsx
@@ -2,8 +2,17 @@ import React from "react";
 
 export default function Advertisement() {
     return (
-    <div>
-        Advertisement
+    <div className="ads-restaurant-frame">
+        <video
+        className={"ads-video"}
+        autoPlay={true}
+        loop
+        muted
+        playsInline
+        data-video-media=""
+        >
+            <source type="video/mp4" src="video/burak-ads.mp4" />
+        </video>
     </div>
     ); 
 }
