44c67de80fbd4fd7df319c3069c30e2db5d3f9ef
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-12 14:32:22 +0900

    feat: develop productPage system Routing

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,6 +1,22 @@
+import React from "react";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
 
 export default function ProductsPage() {
-    return <Container>ProductsPage</Container>;
+    const products = useRouteMatch(); 
+    console.log("products:", products)
+
+    return <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productsId`}>
+          <ChosenProduct/>
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
   }
   
