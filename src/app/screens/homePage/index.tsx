6b700c2f764b2e1fcfec63e347ab581ed494434f
IsmoilovMax <maxmudjon2121@gmail.com>
2024-08-03 00:22:25 +0900

    fix: fix

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,68 +1,70 @@
 import React, { useEffect } from "react";
-import ActiveUsers from "./ActiveUsers";
-import Advertisement from "./Advertisement";
-import Events from "./Events";
-import NewDishes from "./NewDishes";
-import PopularDishes from "./PopularDishes";
 import Statistics from "./Statistics";
-import "../../../css/home.css"
-
-
-import { useDispatch} from "react-redux";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
-import { Member } from "../../../lib/types/member";
 import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
+import "../../../css/home.css";
 
-/** Redux slice & Selector */
+/** Redux Slice & Selector*/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
   setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
- 
 
 export default function HomePage() {
   const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(
     useDispatch()
   );
-  
-  //2:selector: store => Data
-
-  console.log("REACT_APP_API_URL",process.env.REACT_APP_API_URL)
 
   useEffect(() => {
     const product = new ProductService();
     product
-    .getProducts({
-      page: 1,
-      limit: 4,
-      order: "createdAt",
-    })
-    .then((data) => setNewDishes(data))
-    .catch((err) => console.log(err));
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+      })
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
 
     const member = new MemberService();
     member
-    .getTopUsers()
-    .then((data) => setTopUsers(data))
-    .catch((err) => console.log(err));
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
+      .catch((err) => console.log(err));
   }, []);
-  
-  console.log("popular:", PopularDishes)
 
-    return (<div className={"homepage"}>
+  return (
+    <div className={"homepage"}>
       <Statistics />
       <PopularDishes />
       <NewDishes />
       <Advertisement />
       <ActiveUsers />
       <Events />
-      
     </div>
-    );
+  );
 }
-  
